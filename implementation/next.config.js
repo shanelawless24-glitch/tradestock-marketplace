da2a50cf1b41e497ca4ec7eb65e2f041f375a57c
Shane Lawless <shanelawless24@gmail.com>
2026-02-20 23:16:28 +0000

    fix next config clean overwrite

--- implementation/next.config.js
+++ implementation/next.config.js
@@ -1,31 +1,26 @@
+const path = require("path")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['stripe'],
+    appDir: true,
+    serverComponentsExternalPackages: ["stripe"],
   },
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '*.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
+        protocol: "https",
+        hostname: "*.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-};
 
-module.exports = nextConfig;
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src")
+    return config
+  },
+}
+
+module.exports = nextConfig
